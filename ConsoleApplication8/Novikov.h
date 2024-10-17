4728931f7a2df23ad6e3bee1390f0effa019d03a
Enzowax <144791348+Enzowax@users.noreply.github.com>
2024-10-17 08:35:13 +0300

    Update Novikov.h

--- ConsoleApplication8/Novikov.h
+++ ConsoleApplication8/Novikov.h
@@ -1,20 +1,35 @@
 #pragma once
+#include <algorithm> 
+#include <sstream>   
 #include <string>
-#include <algorithm>  
 
-using namespace std;
-
-void SortArray(float arr[], int size)
+std::string SortArray(const float arr[], int size)
 {
-    sort(arr, arr + size); 
+    float* sortedArr = new float[size]; 
+
+    for (int i = 0; i < size; ++i) {
+        sortedArr[i] = arr[i];
+    }
+
+    std::sort(sortedArr, sortedArr + size);
+
+    std::ostringstream result;
+    for (int i = 0; i < size; ++i) {
+        result << sortedArr[i] << " ";
+    }
+
+    delete[] sortedArr;  
+    return result.str();  
 }
 
-float* DuplicateArray(const float arr[], int size)
+std::string DuplicateArray(const float arr[], int size)
 {
-    float* newArr = new float[size];  
-    for (int i = 0; i < size; ++i)
-    {
-        newArr[i] = arr[i];  
+    std::ostringstream result;
+
+
+    for (int i = 0; i < size; ++i) {
+        result << arr[i] << " ";
     }
-    return newArr;  
+
+    return result.str(); 
 }
