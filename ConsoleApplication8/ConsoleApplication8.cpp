ecc8c9be7da317c7fe6f755476e30f7aaacd0d34
Enzowax <144791348+Enzowax@users.noreply.github.com>
2024-10-17 08:35:26 +0300

    Update ConsoleApplication8.cpp

--- ConsoleApplication8/ConsoleApplication8.cpp
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -1,36 +1,19 @@
-﻿#include <iostream>
+#include <iostream>
 #include <windows.h>    
 #include "Novikov.h" 
-
 using namespace std;
 
 int main() {
+
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
     const int size = 5;
     float arr[size] = { 4, 2, 9, 8, 7 };
 
-    cout << "Исходный массив: ";
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+    cout << "Отсортированный массив: " << SortArray(arr, size) << endl;
 
-    SortArray(arr, size);
-    cout << "Отсортированный массив: ";
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    float* duplicatedArray = DuplicateArray(arr, size);
-
-    cout << "Дублированный массив: ";
-    for (int i = 0; i < size; i++) {
-        cout << duplicatedArray[i] << " ";
-    }
-    cout << endl;
+    cout << "Дублированный массив: " << DuplicateArray(arr, size) << endl;
 
     return 0;
 }
